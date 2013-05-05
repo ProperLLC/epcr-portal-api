191bc4a9073528265fe33ee87d9cda5997c5d8a0
Terry Drozdowski <tdrozdowski@gmail.com>
2013-05-04 22:11:05 -0700

    updating project to latest/greatest

--- project/Build.scala
+++ project/Build.scala
@@ -1,21 +1,22 @@
+import _root_.sbt.Keys._
+import _root_.scala._
 import sbt._
 import Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "hermes"
+  val appName         = "epcr-portal-api"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "org.reactivemongo" %% "reactivemongo" % "0.9",
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.9"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    resolvers += "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
   )
 
 }
